f624b21b94a27e34e1bbb198f8724a7830e5042e
Jgooso <joshgoodman0220@gmail.com>
2020-02-28 10:11:16 -0800

    h

---
+++ unit9/ListOddToEven.java
@@ -0,0 +1,33 @@
+package unit9;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOddToEven
+{
+	public static int go( List<Integer> ray )
+	{
+		int odd = -1;
+		int even = 0;
+		for(int i = 0; i < ray.size(); i ++) {
+			if(odd < 0 && ray.get(i) % 2 == 1) {
+				odd = i;
+				//return i;
+			}
+			if( odd > -1 && ray.get(i) % 2 == 0) {
+				even = i;
+				//return i;
+			}
+		}
+		if(even == 0) {
+		return -1;
+		}else {
+			return even - odd;
+		}
+		
+	}
+}
---
+++ unit9/ListSumFirst.java
@@ -0,0 +1,25 @@
+package unit9;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListSumFirst
+{
+	public static int go(List<Integer> ray)
+	{
+		int count = 0;
+		for(int i = 0; i < ray.size(); i++) {
+			if(ray.get(i) > ray.get(0)) {
+				count += ray.get(i);
+			}
+		}
+		if(count == 0) {
+			return -1;
+		}
+		return count;
+	}
+}
